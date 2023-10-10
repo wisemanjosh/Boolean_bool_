5e18344c96eca6625f864f8f70fc08f1cfc833bc
JosueLessie <joslessie@hotmail.com>
2023-10-10 21:11:46 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Declare a variable is_raining and set it to True if it's currently raining in London or False if it's not.
+# Create a boolean variable is_sunny and set it to True if it's sunny.
+
+is_raining = False  # Change to true if it's raining
+
+is_sunny = True  # Change to false if it's not sunny
